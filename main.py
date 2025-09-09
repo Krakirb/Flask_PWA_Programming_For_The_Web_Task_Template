10810a16f472d57e21b62d17dc243bb243529d18
Krakirb <jason.tm.chen@gmail.com>
2025-09-09 10:23:13 +1000

    Adjusting

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import database_manager as dbHandler
 
 app = Flask(__name__)
 
+#Home
 
 @app.route('/index.html', methods=['GET'])
 @app.route('/', methods=['POST', 'GET'])
@@ -12,12 +13,18 @@ def index():
     data = dbHandler.listListing()
     return render_template('/index.html', content=data)
 
+#Log_In
+@app.route('/log_in.html', methods=['GET', 'POST'])
+def log_in():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        # NOT DONE
+        return "Login attempt received"
+    return render_template('log_in.html')
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
-
-
-@app.route('/log_in.html', methods=['GET'])
-@app.route('/', methods=['POST', 'GET'])
-def log_in():
-    return render_template('log_in.html')
